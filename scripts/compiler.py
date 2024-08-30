70b970e9d287b96927cb8f283b0d4545a9e9b8f7
Sabbir Ahmed Shourov <extinctCoder@outlook.com>
2024-08-30 16:25:00 +0600

    feat: added latex pre-compiler

--- scripts/compiler.py
+++ scripts/compiler.py
@@ -1,1 +1,54 @@
+import os
+from os import listdir
+from os.path import isfile, join
+from pathlib import Path
+
+from jinja2 import Environment, FileSystemLoader
 from log_x import log_arbiter
+
+logger = log_arbiter(__name__)
+
+
+LATEX_COMPILER = Environment(
+    block_start_string=r"\BLOCK{",
+    block_end_string="}",
+    variable_start_string=r"\VAR{",
+    variable_end_string="}",
+    comment_start_string=r"\#{",
+    comment_end_string="}",
+    line_statement_prefix="%%",
+    line_comment_prefix="%#",
+    trim_blocks=True,
+    autoescape=False,
+)
+
+
+def compile_latex(resume_data: dict, template_dir: str, output_dir: str) -> None:
+    try:
+        output_dir = Path(output_dir)
+        output_dir.mkdir(parents=True, exist_ok=True)
+        LATEX_COMPILER.loader = FileSystemLoader(template_dir)
+
+        template_files = [
+            file_name
+            for file_name in listdir(template_dir)
+            if isfile(join(template_dir, file_name))
+        ]
+
+        for template_file in template_files:
+            try:
+                template = LATEX_COMPILER.get_template(template_file)
+                rendered_tex = template.render(resume_data)
+                with (output_dir / template_file).open("w") as file:
+                    file.write(rendered_tex)
+                logger.debug(f"Generated: {template_file}")
+            except Exception as ex:
+                logger.error(ex)
+
+        logger.info(f"Generated/Updated {len(template_files)} files in: {output_dir}")
+    except Exception as ex:
+        logger.error(ex)
+
+
+if __name__ == "__main__":
+    logger.info(f"Hello from compiler.py please run main.py to start the script")
--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from click import command, option
+from compiler import compile_latex
 from log_x import log_arbiter
 from utils import read_sha, update_sha
 from yaml import safe_load
@@ -10,7 +11,9 @@ logger = log_arbiter(__name__)
 
 @command
 @option("--resume", "-r", default="../resume.yml", help="location of resume.yml file")
-def main(resume: str):
+@option("--template", "-t", default="../templates", help="Latex TEMPLATE directory")
+@option("--output", "-o", default="../latex", help="Latex OUTPUT directory")
+def main(resume: str, template: str, output: str):
     try:
         resume_path = Path(resume).resolve()
         if not resume_path.is_file():
@@ -22,8 +25,12 @@ def main(resume: str):
         if not resume_data["sha"] or resume_data["sha"] != sha:
             logger.info("RESUME IS OUT-OF-DATE or FIRST RUN")
             update_sha(resume_data=resume_data, sha=sha, resume_path=resume_path)
+            logger.debug("Starting LaTeX compiler to compile resume")
+            compile_latex(
+                resume_data=resume_data, template_dir=template, output_dir=output
+            )
+            logger.info("RESUME COMPILE COMPILED SUCCESSFULLY")
             return
-
         logger.info("RESUME IS UP-TO-DATE")
     except Exception as ex:
         logger.error(ex)
